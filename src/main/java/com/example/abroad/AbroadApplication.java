0eaad0034b6a9f888a74099f5a6687c116af3095
Delali Cudjoe <edc39@duke.edu>
2025-01-17 22:11:39 -0500

    moved controller to controller folder

--- src/main/java/com/example/abroad/AbroadApplication.java
+++ src/main/java/com/example/abroad/AbroadApplication.java
@@ -1,36 +1,11 @@
 package com.example.abroad;
 
-import com.example.abroad.model.Student;
-import com.example.abroad.respository.StudentRepository;
-import java.util.ArrayList;
-import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 
 
 @SpringBootApplication
-@Controller
 public class AbroadApplication {
-	@Autowired
-	private StudentRepository studentRepository;
-
-	@GetMapping("/")
-    public String helloWorld() {
-        return "Hello World";
-    }
-	
-	@GetMapping("/students")
-	public String getStudents(Model model) {
-		studentRepository.save(new Student("johnny123", "secure", "johnny@gmail.com", "John Smith"));
-		studentRepository.save(new Student("jane123", "password", "jane@gmail.com", "Jane Doe"));
-		var students = studentRepository.findAll();
-		model.addAttribute("students", students);
-		return "students";
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AbroadApplication.class, args);
---
+++ src/main/java/com/example/abroad/controller/StudentsController.java
@@ -0,0 +1,31 @@
+package com.example.abroad.controller;
+
+import com.example.abroad.model.Student;
+import com.example.abroad.respository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class StudentsController {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+
+    @GetMapping("/")
+    public String helloWorld() {
+        return "hello";
+    }
+
+    @GetMapping("/students")
+    public String getStudents(Model model) {
+        studentRepository.save(new Student("johnny123", "secure", "johnny@gmail.com", "John Smith"));
+        studentRepository.save(new Student("jane123", "password", "jane@gmail.com", "Jane Doe"));
+        var students = studentRepository.findAll();
+        model.addAttribute("students", students);
+        return "students";
+    }
+
+}
