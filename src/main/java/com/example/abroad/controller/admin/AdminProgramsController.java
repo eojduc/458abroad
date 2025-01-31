1919da7e7341319a365bdb1a467e50121d6bf4b4
Abhishek Chataut <ac802@duke.edu>
2025-01-30 20:50:14 -0500

    added /admin/programs

--- src/main/java/com/example/abroad/controller/admin/AdminProgramsController.java
+++ src/main/java/com/example/abroad/controller/admin/AdminProgramsController.java
@@ -1,32 +1,30 @@
 package com.example.abroad.controller.admin;
 
-import com.example.abroad.respository.AdminRepository;
-import com.example.abroad.respository.StudentRepository;
 import com.example.abroad.service.AdminProgramInfoService;
 import com.example.abroad.service.FormatService;
-import com.example.abroad.service.UserService;
-import jakarta.servlet.http.HttpServletRequest;
-import java.util.Optional;
+import jakarta.servlet.http.HttpSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public record AdminProgramsController(StudentRepository studentRepository, UserService userService, AdminRepository adminRepository, AdminProgramInfoService service, FormatService formatter) {
+public record AdminProgramsController(AdminProgramInfoService service, FormatService formatter) {
+
+  private static final Logger logger = LoggerFactory.getLogger(AdminProgramsController.class);
+
   @GetMapping("/admin/programs")
-  public String getApplicationInfo(HttpServletRequest request, Model model) {
-    var students = studentRepository.findAll();
-    var admins = adminRepository.findAll();
-    // here until auth is set up, we'll just set the first student as the user
-//  students.stream().findFirst().ifPresent(student -> userService.setUser(request, student));
-    admins.stream().findFirst().ifPresent(admin -> userService.setUser(request, admin));
-    var name = Optional.ofNullable(request.getSession().getAttribute("name"))
-        .filter(obj -> obj instanceof String)
-        .map(obj -> (String) obj)
-        .orElse("world");
-    model.addAttribute("name", name);
+  public String getApplicationInfo(HttpSession session,
+      Model model) {
+    var user = service.userService().getUser(session).orElse(null);
+    var programs = service.programRepository().findAll();
+    logger.info("Getting programs for user: {}", user.displayName());
+    logger.info("{}", programs);
+    model.addAttribute("name", user.displayName());
+    model.addAttribute("programs", programs);
+
     return "admin/programs :: page";
   }
+
 }
