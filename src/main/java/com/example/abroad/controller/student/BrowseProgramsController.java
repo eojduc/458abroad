568c726ee455b9dbc43047ee8a4268f828cb33a4
Delali Cudjoe <edc39@duke.edu>
2025-02-17 02:31:07 -0500

    finished refactoring browse programs, updated widths of pages to be responsive to page size

--- src/main/java/com/example/abroad/controller/student/BrowseProgramsController.java
+++ src/main/java/com/example/abroad/controller/student/BrowseProgramsController.java
@@ -1,25 +1,15 @@
 package com.example.abroad.controller.student;
 
 import com.example.abroad.model.Alerts;
-import com.example.abroad.model.Application;
-import com.example.abroad.model.Program;
 import com.example.abroad.service.FormatService;
 import com.example.abroad.service.UserService;
-import com.example.abroad.service.page.admin.BrowseProgramsService;
-import com.example.abroad.service.page.admin.BrowseProgramsService.GetAllProgramsInfo;
-import com.example.abroad.service.page.admin.BrowseProgramsService.GetAllProgramsInfo.Success;
-import com.example.abroad.service.page.admin.BrowseProgramsService.GetAllProgramsInfo.UserNotFound;
-import com.example.abroad.service.page.admin.BrowseProgramsService.Sort;
+import com.example.abroad.service.page.BrowseProgramsService;
+import com.example.abroad.service.page.BrowseProgramsService.GetAllProgramsInfo;
+import com.example.abroad.service.page.BrowseProgramsService.GetAllProgramsInfo.Success;
+import com.example.abroad.service.page.BrowseProgramsService.GetAllProgramsInfo.UserNotFound;
 import jakarta.servlet.http.HttpSession;
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Optional;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -29,71 +19,53 @@ import org.springframework.web.bind.annotation.RequestParam;
 public record BrowseProgramsController(BrowseProgramsService service, FormatService formatter,
                                        UserService userService) {
 
-  private static final Logger logger = LoggerFactory.getLogger(BrowseProgramsController.class);
-
   @GetMapping("/programs")
   public String getProgramsInfo(HttpSession session, Model model,
-      @RequestParam(required = false) String nameFilter,
       @RequestParam Optional<String> error,
       @RequestParam Optional<String> success,
       @RequestParam Optional<String> warning,
       @RequestParam Optional<String> info
   ) {
-    var sort = Sort.APP_CLOSES;
-    var timeFilter = BrowseProgramsService.TimeFilter.FUTURE;
-    boolean noFilter = nameFilter == null;
-    if (noFilter) {
-      service.clearSessionData(session);
-    }
 
-    GetAllProgramsInfo programsInfo = service.getProgramInfo(session, sort, nameFilter, timeFilter,
-        true, true);
+    GetAllProgramsInfo programsInfo = service.getProgramInfo(session, "");
 
     return switch (programsInfo) {
       case UserNotFound() -> "redirect:/login?error=You are not logged in";
-      case Success(var programs, var applications, var user) -> {
-
-        logger.info("User {} \nApplications {} \n", user, applications);
-        // Create the ordered list that will contain the application statuses
-        List<String> programStatus = new ArrayList<>(Collections.nCopies(programs.size(), null));
-
-// Populate status for each program position
-        for (int i = 0; i < programs.size(); i++) {
-          Program program = programs.get(i);
-          LocalDate today = LocalDate.now();
-          // Find if there's an application for this program
-          Application userApp = applications.stream()
-              .filter(app -> Objects.equals(app.student(), user.username())
-                  && Objects.equals(app.programId(), program.id()))
-              .findFirst()
-              .orElse(null);
-
-          if (userApp != null) {
-            // If user has an application, use its status
-            programStatus.set(i, userApp.status().toString());
-          } else {
-            // No application - determine why
-            if (today.isBefore(program.applicationOpen())) {
-              programStatus.set(i, "NotOpen");
-            } else if (today.isAfter(program.applicationClose())) {
-              programStatus.set(i, "DeadPass");
-            } else {
-              programStatus.set(i, "Open");
-            }
-          }
-        }
+      case Success(var programAndStatuses, var user) -> {
 
         model.addAllAttributes(
             Map.of(
                 "user", user,
-                "programs", programs,
-                "programStatus", programStatus,
                 "alerts", new Alerts(error, success, warning, info),
+                "programAndStatuses", programAndStatuses,
                 "formatter", formatter,
               "theme", userService.getTheme(session)
             )
         );
-        yield "student/programs :: " + (noFilter ? "page" : "programTable");
+        yield "student/programs :: page";
+      }
+      default -> throw new IllegalStateException("Unexpected value: " + programsInfo);
+    };
+  }
+
+  @GetMapping("/programs/search")
+  public String searchPrograms(HttpSession session, Model model,
+    @RequestParam String nameFilter
+  ) {
+    GetAllProgramsInfo programsInfo = service.getProgramInfo(session, nameFilter);
+
+    return switch (programsInfo) {
+      case UserNotFound() -> "redirect:/login?error=You are not logged in";
+      case Success(var programsAndStatuses, var user) -> {
+        model.addAllAttributes(
+          Map.of(
+            "user", user,
+            "formatter", formatter,
+            "theme", userService.getTheme(session),
+            "programAndStatuses", programsAndStatuses
+          )
+        );
+        yield "student/programs :: programTable";
       }
       default -> throw new IllegalStateException("Unexpected value: " + programsInfo);
     };
--- src/main/java/com/example/abroad/service/page/BrowseProgramsService.java
+++ src/main/java/com/example/abroad/service/page/BrowseProgramsService.java
@@ -1,5 +1,5 @@
 
-package com.example.abroad.service.page.admin;
+package com.example.abroad.service.page;
 
 import com.example.abroad.model.Application;
 import com.example.abroad.model.Program;
@@ -7,15 +7,13 @@ import com.example.abroad.model.User;
 import com.example.abroad.respository.ApplicationRepository;
 import com.example.abroad.respository.ProgramRepository;
 import com.example.abroad.service.UserService;
+import com.example.abroad.service.page.BrowseProgramsService.GetAllProgramsInfo.Success;
+import com.example.abroad.service.page.BrowseProgramsService.GetAllProgramsInfo.UserNotFound;
 import jakarta.servlet.http.HttpSession;
 import java.time.LocalDate;
-import java.util.Comparator;
 import java.util.List;
-import java.util.Map;
+import java.util.function.Function;
 import java.util.function.Predicate;
-import java.util.stream.Collectors;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -24,174 +22,85 @@ public record BrowseProgramsService(
   ApplicationRepository applicationRepository,
   UserService userService
 ) {
-
-  private static final Logger logger = LoggerFactory.getLogger(AdminProgramsService.class);
-  private static final TimeFilter DEFAULT_TIME_FILTER = TimeFilter.FUTURE;
-  public enum Sort {
-    TITLE,
-    SEM_DATE,
-    APP_OPENS,
-    APP_CLOSES,
-    START_DATE,
-    END_DATE,
-    FACULTY_LEAD,
-    APPLIED,
-    ENROLLED,
-    CANCELED,
-    WITHDRAWN,
-    COUNT
-  }
-  public enum TimeFilter {
-    FUTURE,
-    OPEN,
-    REVIEW,
-    RUNNING,
-    ALL
-  }
   public GetAllProgramsInfo getProgramInfo(
     HttpSession session,
-    Sort sort,
-    String nameFilter,
-    TimeFilter timeFilter,
-    boolean studentMode,
-    Boolean ascending
+    String nameFilter
   ) {
     return userService.findUserFromSession(session)
-      .map(user -> processAuthorizedRequest(session, sort, nameFilter, timeFilter, user,
-        studentMode, ascending))
-      .orElse(new GetAllProgramsInfo.UserNotFound());
+      .map(user -> processAuthorizedRequest(nameFilter, user))
+      .orElse(new UserNotFound());
   }
-
-  public void clearSessionData(HttpSession session) {
-    session.removeAttribute("nameFilter");
-    session.removeAttribute("timeFilter");
-    session.removeAttribute("lastSort");
-    session.removeAttribute("title");
-    session.removeAttribute("appOpens");
-    session.removeAttribute("appCloses");
-    session.removeAttribute("semDate");
-    session.removeAttribute("startDate");
-    session.removeAttribute("endDate");
-    session.removeAttribute("applied");
-    session.removeAttribute("enrolled");
-    session.removeAttribute("canceled");
-    session.removeAttribute("withdrawn");
-    session.removeAttribute("count");
-    session.removeAttribute("facultyLead");
-    session.removeAttribute("totalActive");
-  }
-
   private GetAllProgramsInfo processAuthorizedRequest(
-    HttpSession session,
-    Sort sort,
     String nameFilter,
-    TimeFilter timeFilter,
-    User user,
-    boolean studentMode,
-    Boolean ascending
+    User user
   ) {
-    if (!studentMode && user.role() != User.Role.ADMIN) {
-      return new GetAllProgramsInfo.UserNotAdmin();
-    }
 
-    List<Program> programs = programRepository.findAll();
-    List<Application> studentApplications = applicationRepository.findProgramApplicationsByStudent(
-        user.username())
-      .stream()
-      .map(opt -> opt.orElseGet(
-        Application::new)) // Replace empty optionals with a new Application
-      .toList();
-
-    List<Application> allApplications = applicationRepository.findAll();
-    var comparator = getSortComparator(sort, allApplications, programs);
-    var programsToDisplay = programs.stream()
-      .filter(matchesNamePredicate(nameFilter))
-      .filter(getTimeFilterPredicate(timeFilter))
-      .sorted(ascending ? comparator : comparator.reversed())
-      .toList();
-
-    return new GetAllProgramsInfo.Success(
-      programsToDisplay,
-      studentMode ? studentApplications : allApplications,
+    return new Success(
+      programRepository.findAll().stream()
+        .filter(matchesNamePredicate(nameFilter))
+        .map(getProgramAndStatus(user))
+        .toList(),
       user
     );
   }
 
+  public Function<Program, ProgramAndStatus> getProgramAndStatus(User user) {
+    return program -> {
+      var applicationStatus = applicationRepository.findByProgramIdAndStudent(program.id(), user.username())
+        .map(Application::status)
+        .orElse(null);
 
-  public Map<Integer, Map<String, Integer>> getProgramStatus(List<Application> applications, List<Program> programs) {
-    // Group applications by program ID
-    Map<Integer, List<Application>> groupedApplications = applications.stream()
-      .collect(Collectors.groupingBy(Application::programId));
-
-    // Process in order of programs and store in a map
-    return programs.stream()
-      .collect(Collectors.toMap(
-        Program::id,  // Key: programId
-        program -> {
-          List<Application> applicationList = groupedApplications.getOrDefault(program.id(), List.of());
-
-          int applied = 0, enrolled = 0, canceled = 0, withdrawn = 0;
-          for (Application app : applicationList) {
-            switch (app.status()) {
-              case APPLIED -> applied++;
-              case ENROLLED -> enrolled++;
-              case CANCELLED -> canceled++;
-              case WITHDRAWN -> withdrawn++;
+      return new ProgramAndStatus(
+        program,
+        switch (applicationStatus) {
+          case APPLIED -> ProgramStatus.APPLIED;
+          case ELIGIBLE -> ProgramStatus.ELIGIBLE;
+          case APPROVED -> ProgramStatus.APPROVED;
+          case CANCELLED -> ProgramStatus.CANCELLED;
+          case WITHDRAWN -> ProgramStatus.WITHDRAWN;
+          case ENROLLED -> program.endDate().isBefore(LocalDate.now()) ? ProgramStatus.COMPLETED : ProgramStatus.ENROLLED;
+          case null -> {
+            if (program.applicationOpen().isAfter(LocalDate.now())) {
+              yield ProgramStatus.UPCOMING;
+            } else if (program.applicationClose().isBefore(LocalDate.now())) {
+              yield ProgramStatus.CLOSED;
+            } else {
+              yield ProgramStatus.OPEN;
             }
           }
-          int count = applied + enrolled;
-          return Map.of(
-            "applied", applied,
-            "enrolled", enrolled,
-            "canceled", canceled,
-            "withdrawn", withdrawn,
-            "count", count
-          );
         }
-      ));
+      );
+    };
   }
+
+  public enum ProgramStatus {
+    APPLIED,
+    ENROLLED,
+    CANCELLED,
+    WITHDRAWN,
+    ELIGIBLE,
+    APPROVED,
+    COMPLETED,
+    UPCOMING,
+    OPEN,
+    CLOSED
+  }
+  public record ProgramAndStatus(
+    Program program,
+    ProgramStatus status
+  ) {
+  }
+
 
   private Predicate<Program> matchesNamePredicate(String searchTerm) {
     return program -> program.title().toLowerCase().contains(searchTerm.toLowerCase());
     // || program.facultyLead().toLowerCase().contains(searchTerm)
   }
 
-  private Predicate<Program> getTimeFilterPredicate(TimeFilter timeFilter) {
-    LocalDate today = LocalDate.now();
-    return switch (timeFilter) {
-      case FUTURE -> program -> !(program.endDate().isBefore(today));
-      case OPEN -> program -> !(program.applicationOpen().isBefore(today) || program.applicationClose().isAfter(today));
-      case REVIEW -> program -> !(program.applicationClose().isAfter(today) || program.startDate().isBefore(today));
-      case RUNNING -> program -> !(program.startDate().isAfter(today) || program.endDate().isBefore(today));
-      case ALL -> program -> true;
-    };
-  }
-  private Comparator<Program> getSortComparator(Sort sort, List<Application> applications, List<Program> programs) {
-    Map<Integer, Map<String, Integer>> programStatus = getProgramStatus(applications, programs);
-    return switch (sort) {
-      case TITLE -> Comparator.comparing(Program::title);
-      case SEM_DATE -> Comparator.comparing(Program::year).thenComparing(Program::semester);
-      case APP_OPENS -> Comparator.comparing(Program::applicationOpen);
-      case APP_CLOSES -> Comparator.comparing(Program::applicationClose);
-      case START_DATE -> Comparator.comparing(Program::startDate);
-      case END_DATE -> Comparator.comparing(Program::endDate);
-      case FACULTY_LEAD -> Comparator.comparing(Program::title);
-      case APPLIED -> Comparator.comparingInt(program -> programStatus.get(program.id()).get("applied"));
-      case ENROLLED -> Comparator.comparingInt(program -> programStatus.get(program.id()).get("enrolled"));
-      case CANCELED -> Comparator.comparingInt(program -> programStatus.get(program.id()).get("canceled"));
-      case WITHDRAWN -> Comparator.comparingInt(program -> programStatus.get(program.id()).get("withdrawn"));
-      case COUNT -> Comparator.comparingInt(program -> programStatus.get(program.id()).get("count"));
-    };
-  }
-
-  public sealed interface GetAllProgramsInfo permits
-    GetAllProgramsInfo.Success,
-    GetAllProgramsInfo.UserNotFound,
-    GetAllProgramsInfo.UserNotAdmin {
+  public sealed interface GetAllProgramsInfo {
 
     record Success(
-      List<Program> programs,
-      List<Application> applicants,
+      List<ProgramAndStatus> programAndStatuses,
       User user
     ) implements GetAllProgramsInfo {
 
