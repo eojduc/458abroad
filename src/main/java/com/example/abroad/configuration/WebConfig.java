283d598e5efae8c49c4a93a235d6ad42173c15bc
Delali Cudjoe <edc39@duke.edu>
2025-02-27 17:39:37 -0500

    cosmetic

--- src/main/java/com/example/abroad/configuration/WebConfig.java
+++ src/main/java/com/example/abroad/configuration/WebConfig.java
@@ -8,13 +8,6 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;  // Add this import
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-/**
- * Add resource handlers to serve static images.
- *
- * <p>This method configures Spring MVC to serve static images from the
- * classpath. The images are located in the "static/images" directory and can be accessed via the
- * "/images/**" URL pattern.
- */
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
