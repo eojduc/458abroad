3bbb8f2ff47c169c845bbfbdcd51ec003962f4d5
Delali Cudjoe <edc39@duke.edu>
2025-03-25 15:01:28 -0400

    updated alerts to be bottom right

--- src/main/java/com/example/abroad/service/page/DashboardService.java
+++ src/main/java/com/example/abroad/service/page/DashboardService.java
@@ -27,7 +27,7 @@ public record DashboardService(UserService userService, SSOService ssoService) {
     if (user == null) {
       return new GetDashboard.NotLoggedIn();
     }
-    if (userService.isAdmin(user)) {
+    if (!userService.isStudent(user)) {
       return new GetDashboard.AdminDashboard(user);
     }
     return new GetDashboard.StudentDashboard(user);
